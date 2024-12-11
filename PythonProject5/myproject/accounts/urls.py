b22bc27f7879a020ab48ebcd9a04ba4a4c467a0a
eunyyda <psen1207@gmail.com>
2024-12-11 23:45:31 +0900

    Delete PythonProject5 directory

--- PythonProject5/myproject/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('signup/', views.signup, name='signup'),  # 회원가입 URL
-    path('', views.home, name='home'),
-]
